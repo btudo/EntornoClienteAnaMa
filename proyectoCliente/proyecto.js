25ba7a85be5b7c2ea5b516807f6009cb051659f3
btudo.ceu <btudo@ceuandalucia.es>
2023-10-31 22:23:41 +0100

    Añado comentarios

--- proyectoCliente/proyecto.js
+++ proyectoCliente/proyecto.js
@@ -7,78 +7,80 @@ var posicion_tortuga = 1;
 //inicializamos nuestra variable temporizador
 var temporizador;
 
-
 //colocamos nuestras imagenes en la meta de salida
 var comenzar = () => {
-    //El porcentaje se usa para que nuestro elemento se mueva un 1% cogiendo de referencia nuestro contenedor
-    pez.style.left = "1%";
-    tortuga.style.left = "1%";
-    //volvemos a poner nuestras variables iniciales en 1
-    posicion_pez = 1;
-    posicion_tortuga = 1;
-
-}
+  //El porcentaje se usa para que nuestro elemento se mueva un 1% cogiendo de referencia nuestro contenedor
+  pez.style.left = "1%";
+  tortuga.style.left = "1%";
+  //volvemos a poner nuestras variables iniciales en 1
+  posicion_pez = 1;
+  posicion_tortuga = 1;
+};
 var empate = () => {
+  document.getElementById("boton2").disabled = true;
+  document.getElementById("boton3").disabled = true;
+  //estamos diciendo que nuestra posicion tiene que estar dentro de nuestra pantalla
+  if (posicion_pez <= 95 && posicion_pez >= 1) {
+    //Belén. AMpliado a 95
+    posicion_pez = posicion_pez + 1;
+    pez.style.left = posicion_pez + "%";
+    // si no está en 1 coloca nuestra posición en 1
+  } else {
+    posicion_pez = 1;
+    pez.style.left = posicion_pez + "%";
+  }
 
-    document.getElementById("boton2").disabled = true;
-    document.getElementById("boton3").disabled = true;
-//estamos diciendo que nuestra posicion tiene que estar dentro de nuestra pantalla
-    if (posicion_pez <= 85 && posicion_pez >= 1) {
-        posicion_pez = posicion_pez + 1;
-        pez.style.left = posicion_pez + "%";
-// si no está en 1 coloca nuestra posición en 1
-    } else {
-        posicion_pez = 1;
-        pez.style.left = posicion_pez + "%";
-    }
-    
-    if (posicion_tortuga <= 85 && posicion_tortuga >= 1) {
-        posicion_tortuga = posicion_tortuga + 1;
-        tortuga.style.left = posicion_tortuga + "%";
-
-    } else {
-        posicion_tortuga = 1;
-        tortuga.style.left = posicion_tortuga + "%";
-    }
-}
+  if (posicion_tortuga <= 95 && posicion_tortuga >= 1) {
+    //Belén. AMpliado a 95
+    posicion_tortuga = posicion_tortuga + 1;
+    tortuga.style.left = posicion_tortuga + "%";
+  } else {
+    posicion_tortuga = 1;
+    tortuga.style.left = posicion_tortuga + "%";
+  }
+};
 var ganador = () => {
-//deshabilitamos los botones 
-    document.getElementById("boton2").disabled = true;
-    document.getElementById("boton3").disabled = true;
-//repetimos lo mismo de arriba
-    if (posicion_pez <= 85 && posicion_pez >= 1) {
-        posicion_pez = posicion_pez + 2;
-        pez.style.left = posicion_pez + "%";
-    } else {
-        posicion_pez = 1;
-        pez.style.left = posicion_pez + "%";
-    }
+  //deshabilitamos los botones
+  document.getElementById("boton2").disabled = true;
+  document.getElementById("boton3").disabled = true;
+  //repetimos lo mismo de arriba
+  if (posicion_pez <= 95 && posicion_pez >= 1) {
+    //Belén. AMpliado a 95
+    posicion_pez = posicion_pez + 2;
+    pez.style.left = posicion_pez + "%";
+  } else {
+    posicion_pez = 1;
+    pez.style.left = posicion_pez + "%";
+  }
 
-    if (posicion_tortuga <= 85 && posicion_tortuga >= 1) {
-        posicion_tortuga = posicion_tortuga + 1;
-        tortuga.style.left = posicion_tortuga + "%";
-    } else {
-        posicion_tortuga = 1;
-        tortuga.style.left = posicion_tortuga + "%";
-    }
-
-}
+  if (posicion_tortuga <= 95 && posicion_tortuga >= 1) {
+    //Belén. AMpliado a 95
+    posicion_tortuga = posicion_tortuga + 1;
+    tortuga.style.left = posicion_tortuga + "%";
+  } else {
+    posicion_tortuga = 1;
+    tortuga.style.left = posicion_tortuga + "%";
+  }
+};
 //iniciamos nuestro temporizador y llamamos a nuestro método empate
 
 var iniciarCarreraEmpate = () => {
-    temporizador = setInterval(empate, 100);
-}
+  temporizador = setInterval(empate, 100);
+};
 //lo iniciamos y llamamos a nuetsro método ganador
 var iniciarCarreraGanador = () => {
-    temporizador = setInterval(ganador, 100);
-}
+  temporizador = setInterval(ganador, 100);
+};
 
 //paramos nuestro temporizador y volvemos a habilitar los botones
 var parar = () => {
-    clearInterval(temporizador);
-    
-    document.getElementById("boton2").disabled = false;
-    document.getElementById("boton3").disabled = false;
-    
+  clearInterval(temporizador);
 
-}
+  document.getElementById("boton2").disabled = false;
+  document.getElementById("boton3").disabled = false;
+};
+
+/*
+Muy bien Ana,
+Sólo te he subido a 95 la comparación en el if para que llegue más al final de la pantalla., :-)
+*/
